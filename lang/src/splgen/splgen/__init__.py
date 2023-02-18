fc75ba99736f399650ef8b46138e6f2d57abc46e
albertmakan <makanalbert@gmail.com>
2023-02-18 23:42:32 +0000

    feat: #4 added some model classes and object processors

--- lang/src/splgen/splgen/__init__.py
+++ lang/src/splgen/splgen/__init__.py
@@ -1,6 +1,6 @@
 import os
-from functools import partial
-from textx import generator, gen_file, get_output_filename
+from spl import Script
+from textx import generator
 
 __version__ = "0.1.0.dev"
 
@@ -9,16 +9,14 @@ __version__ = "0.1.0.dev"
 def spl_generate_sol(metamodel, model, output_path, overwrite, debug, **custom_args):
     "Generator for generating sol from spl descriptions"
 
-    output_file = get_output_filename(model.file_name, output_path, '*.spl')
-    gen_file(model.file_name, output_file,
-             partial(generator_callback, model, output_file),
-             overwrite,
-             success_message='To convert to png run "dot -Tpng -O {}"'
-             .format(os.path.basename(output_file)))
+    output_file = "proba1"
+    generator_callback(model, output_file, overwrite)
 
 
-def generator_callback(model, output_file):
+def generator_callback(model: Script, output_file: str, overwrite: bool):
     """
     A generator function that produce output_file from model.
     """
     # TODO: Write here code that produce generated output
+    with open(output_file, 'w') as f:
+        f.write(str(model))
