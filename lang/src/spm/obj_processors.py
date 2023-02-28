a4edc04053cf66845660720487014c3b858e1b7c
draganaF <filipovic.dada@gmail.com>
2023-02-28 20:26:21 +0100

    feat: #1 supported local packages from package.json

--- lang/src/spm/obj_processors.py
+++ lang/src/spm/obj_processors.py
@@ -1,50 +1,56 @@
 import os
 from typing import List
-from model import Construct, PackageExport, PackageExportSection, PackageImportSection, PackageImport, Script
-from os import path
+from model import PackageExport, PackageExportSection, PackageImportSection, PackageImport
 
 from sol_code_extractions import extract_sol_data
 from sol_dependency_analysers import form_dependencies
 from merge import merge_data_with_dependencies, merge_data
+from file_utils import load_local_packages, load_solidity_file, load_package, check_file_path, is_file
+from model_utils import find_import, find_root, compute_package_alias, compute_exported_name
 
 
 solidity_files = { }
+local_packages = load_local_packages()
+
+# -------------------- PACKAGE IMPORT SECTION PROCESSOR --------------------
 
 def package_import_section_processor(package_section: PackageImportSection):
     names = []
-    for pack in package_section.packages:
-        if not pack.alias:
-            pack.alias = _compute_pack_alias(pack)
-        if pack.alias in names:
-            raise Exception(f"Multiple imports with same name: '{pack.alias}'")
-        names.append(pack.alias)
+    for package in package_section.packages:
+        if package.alias in names:
+            raise Exception(f"Multiple imports with same name: '{package.alias}'")
+        names.append(package.alias)
 
-def _compute_pack_alias(pack: PackageImport):
-    isFileImport = _is_file_import(pack.id)
-    if isFileImport:
-        return pack.id.replace('"', '').split('/')[-1].replace('.sol', '')
-    
-    return pack.id.split('.')[-1]
+# ------------------------ PACKAGE IMPORT PROCESSOR -------------------------
 
 def package_import_processor(package_import: PackageImport):
+    package_import.alias = compute_package_alias(package_import)
 
+    isFileImport = is_file(package_import.id)
+    if isFileImport:
+        process_imported_solidity_file(package_import)
+        return
+    
+    process_imported_spm_package(package_import)
+
+def process_imported_solidity_file(package_import: PackageImport):
     sol_data = {}
     load_queue = [package_import.id]
 
     while len(load_queue):
         file_path = load_queue.pop()
-        isFileImport = _is_file_import(file_path)
+        isFileImport = is_file(file_path)
         if isFileImport:
-            _check_file_path(file_path)
+            check_file_path(file_path)
 
-            input, input_lines = _load_solidity_file(file_path)
+            input, input_lines = load_solidity_file(file_path)
 
             file_sol_data = extract_sol_data(input, input_lines)
             sol_data = merge_data(sol_data, file_sol_data)
 
             parent_file_path = os.path.dirname(file_path.replace('"', ''))
             for imported_file in sol_data['@global']['imports']:
-                if _is_file_import(imported_file):
+                if is_file(imported_file):
                     load_queue.append('"{0}"'.format(os.path.join(parent_file_path, imported_file.replace('"', ''))))
 
     # this is to remove interfaces
@@ -52,33 +58,45 @@ def package_import_processor(package_import: PackageImport):
         if contract_data['base'] and contract_data['base'] not in sol_data:
             contract_data['base'] = None
 
-
-
     dependencies = form_dependencies(sol_data)
     merged_sol_data = merge_data_with_dependencies(sol_data, dependencies)
 
     solidity_files[package_import.alias] = merged_sol_data
     package_import.data = merged_sol_data
 
+def process_imported_spm_package(package_import: PackageImport):
+    package_name = package_import.id.split(".")[0]
+    _check_local_package(package_name)
 
-def _is_file_import(id):
-    return id.startswith('".') and id.endswith('"')
+    package = load_package(package_name, local_packages[package_name])
+    _check_package_namespace(package, package_import.id)
 
-def _check_file_path(filePath):
-    if not path.isfile(filePath.replace('"', '')):
-        raise Exception(f"{filePath} is not correct file path")
+    solidity_files[package_import.alias] = package
+    package_import.data = package
 
-def _load_solidity_file(file_path):
-  with open(file_path.replace('"', ''), "r") as fp:
-    input = fp.read()
-    fp.seek(0)
-    input_lines = fp.readlines()
+def _check_local_package(package_name):
+    if package_name not in local_packages:
+        raise Exception(f"{package_name} is not installed")
 
-    return input, input_lines
+def _check_package_namespace(package, package_namespace):
+    namespace_parts = package_namespace.split(".")
+    package_name = namespace_parts[0]
+    
+    package_definition = package["definition"]
+    current_package = package_definition
+    for namespace_part in namespace_parts:
+        if namespace_part not in current_package:
+            raise Exception(f"{package_namespace} namespace is not defined in {package_name} package")
+
+        current_package = current_package[namespace_part]
+
+        if 'type' in current_package and 'path' in current_package:
+            raise Exception(f"Solidity {current_package['type'][:-1]} {package_namespace} cannot be imported directly")
+
+# -------------------- PACKAGE EXPORT SECTION PROCESSOR --------------------
 
 def package_export_section_processor(package_section: PackageExportSection):
     process_ambiguous_exports(package_section.exports)
-
     print_package_export_section(package_section)
 
 def process_ambiguous_exports(exports: List[PackageExport]):
@@ -91,21 +109,30 @@ def process_ambiguous_exports(exports: List[PackageExport]):
 def try_to_find_ambiguous_export(exports: List[PackageExport]):
     defined_names = []
     for export in exports:
-        name = _compute_exported_name(export)
+        name = compute_exported_name(export)
 
         if name in defined_names:
             raise Exception(f"{name} is already defined")
 
         defined_names.append(name)
 
-def _compute_exported_name(export: PackageExport):
-    if export.package_name:
-        return export.package_name
+def print_package_export_section(package_section: PackageExportSection):
+    print("Main package: " + package_section.name)
+    for export in package_section.exports:
+        print_package_export(export)
 
-    if export.export_alias:
-        return export.export_alias
+def print_package_export(export: PackageExport):
+    if export.export_name:
+        if not export.export_type:
+            export.export_type = '@function'
+        print(export.export_type + ": " + export.export_name)
+        return
 
-    return export.export_name.split('.')[-1]
+    print("Nested package: " + export.package_name)
+    for nested_export in export.exports:
+        print_package_export(nested_export)
+
+# ------------------------ PACKAGE EXPORT PROCESSOR -------------------------
 
 def package_export_processor(export: PackageExport):
     if export.export_name is None and len(export.exports) == 0:
@@ -117,8 +144,17 @@ def package_export_processor(export: PackageExport):
     if not export.export_type:
         export.export_type = '@function'
 
-    export_name_tokens = export.export_name.split('.')
+    imported_package = find_import(export)
+    if imported_package is None:
+        raise Exception(f"Package '{export.export_name.split('.')[0]}' not found")
 
+    if is_file(imported_package.id):
+        process_export_from_file(export)
+    else:
+        process_export_from_package(imported_package, export)
+
+def process_export_from_file(export: PackageExport):
+    export_name_tokens = export.export_name.split('.')
     if len(export_name_tokens) != 2 and export.export_type == '@contract':
         raise Exception(f"Export for contract must be PackageAlias.ContractName")
 
@@ -131,12 +167,39 @@ def package_export_processor(export: PackageExport):
     if len(export_name_tokens) not in [2, 3]:
         raise Exception(f"Export must be PackageAlias[.ContractName].SolidityTypeName")
     
-    _check_if_package_is_imported(export, export_name_tokens[0])
 
     if export.export_type == '@contract':
         _check_if_contract_exists(export)
     if export.export_type in ['@function', '@struct', '@event', '@modifier']:
         _check_if_non_contract_exists(export)
+
+def process_export_from_package(imported_package: PackageImport, export: PackageExport):
+    full_export = "{0}.{1}".format(imported_package.id, export.export_name.replace(imported_package.alias + ".", ""))
+
+    # check namespace up to last .
+    namespace_parts, export_name = full_export.split(".")[:-1], full_export.split(".")[-1]
+    package_name = namespace_parts[0]
+    current_package = imported_package.data["definition"]
+    for i, namespace_part in enumerate(namespace_parts):
+        if namespace_part not in current_package:
+            visided_namespace = '.'.join(namespace_parts[:i+1]) 
+            raise Exception(f"{visided_namespace} namespace is not defined in {package_name} package")
+        current_package = current_package[namespace_part]
+
+        if 'type' in current_package and 'path' in current_package:
+            raise Exception(f"Expected namespace but got solidity {namespace_part} {current_package['type'][:-1]} instead")
+
+    # check export
+    if export_name not in current_package:
+        full_namespace = '.'.join(namespace_parts)
+        raise Exception(f"{export_name} does not exist in {full_namespace} namespace of {package_name} package")
+
+    current_package = current_package[export_name]
+    if 'type' not in current_package and 'path' not in current_package:
+        raise Exception(f"{export_name} is a part of a namespace and not the solidity type")
+
+    if export.export_type != ('@' + current_package['type'][:-1]):
+        raise Exception(f"Expected {export_name} to be {export.export_type[1:]} but is {current_package['type'][:-1]}")
 
 
 def _check_if_contract_exists(export: PackageExport):
@@ -152,16 +215,6 @@ def _check_if_non_contract_exists(export: PackageExport):
         _check_if_contract_exists_in_package(export_name_tokens[0], export_name_tokens[1])
         _check_if_solidity_type_exists_in_package(export_name_tokens[0], export_name_tokens[1], export.export_type, export_name_tokens[2])
 
-
-def _check_if_package_is_imported(export: PackageExport, package_alias: str):
-    root = _find_root(export)
-    if not root:
-        raise Exception("No root")
-
-    pack = next((package for package in root.imports.packages if package.alias == package_alias), None)
-    if not pack:
-        raise Exception(f"Package '{package_alias}' not found")
-
 def _check_if_contract_exists_in_package(package_alias: str, contract_name: str):
     if contract_name not in solidity_files[package_alias]:
         raise Exception(f"Contract '{contract_name}' not found in package '{package_alias}'")
@@ -170,38 +223,8 @@ def _check_if_solidity_type_exists_in_package(package_alias: str, contract_name:
     export_type = export_type.replace('@', '') + 's'
     current_contract_name = contract_name
     while current_contract_name is not None:
-
         if export_name in solidity_files[package_alias][current_contract_name][export_type]:
             return
         
         current_contract_name = solidity_files[package_alias][current_contract_name]['base']
     raise Exception(f"{export_type[:-1]} '{export_name}' not found in contract '{contract_name}' of '{package_alias}' package")
-
-
-
-def print_package_export_section(package_section: PackageExportSection):
-    print("Main package: " + package_section.name)
-    for export in package_section.exports:
-        print_package_export(export)
-
-
-def print_package_export(export: PackageExport):
-    if export.export_name:
-        if not export.export_type:
-            export.export_type = '@function'
-        print(export.export_type + ": " + export.export_name)
-        return
-
-    print("Nested package: " + export.package_name)
-    for nested_export in export.exports:
-        print_package_export(nested_export)
-
-
-def _find_root(construct: Construct) -> Script:
-    c: Construct = construct
-    while True:
-        if 'parent' not in dir(c):
-            break
-        c = c.parent
-        
-    return c
--- lang/src/spmgen/package_code_generator.py
+++ lang/src/spmgen/package_code_generator.py
@@ -3,7 +3,7 @@ def generate_package_code(sol_data, exported_items):
   package = {}
   for exported_item in exported_items:
     contract, sol_item, sol_type = exported_item.split('_')
-    contract_chain = find_contract_chain_upwards(contract, sol_data)
+    contract_chain = _find_contract_chain_upwards(contract, sol_data)
     for contract_in_chain in contract_chain:
       if contract_in_chain not in package:
         package[contract_in_chain] = {
@@ -17,26 +17,19 @@ def generate_package_code(sol_data, exported_items):
 
   return package
 
-def compute_initial_queue_item(sol_data, export):
-  export_tokens = export.split('.')
-
-  if export_tokens[-1] == 'contracts':
-    return [[export_tokens[0]], export_tokens[0], export_tokens[1]]
-
-  return [find_contract_chain_downwards(export_tokens[0], sol_data), export_tokens[1], export_tokens[2]]
 
 def find_all_exported_items(sol_data, exports):
-  queue = [compute_initial_queue_item(sol_data, export) for export in exports]
+  queue = [_compute_initial_queue_item(sol_data, export) for export in exports]
   exported_items = []
 
   while len(queue):
     possible_contracts, item_name, item_type = queue.pop()
 
     if item_type == 'contracts':
-      queue.extend(form_queue_items_for_dependent_contract(sol_data, [item_name]))
+      queue.extend(_form_queue_items_for_dependent_contract(sol_data, [item_name]))
     else:
       for contract in possible_contracts:
-        if not item_name in sol_data[contract][item_type]:
+        if item_type not in sol_data[contract] or item_name not in sol_data[contract][item_type]:
           continue
 
         key = "{0}_{1}_{2}".format(contract, item_name, item_type)
@@ -48,19 +41,27 @@ def find_all_exported_items(sol_data, exports):
 
         for dependency_sol_type, dependency_sol_items in sol_data[contract][item_type][item_name]["dependencies"].items():
           if dependency_sol_type == "contracts":
-            queue.extend(form_queue_items_for_dependent_contract(sol_data, dependency_sol_items))
+            queue.extend(_form_queue_items_for_dependent_contract(sol_data, dependency_sol_items))
           else:
-            queue.extend(form_queue_items_for_dependency_other_then_contract(sol_data, contract, dependency_sol_type, dependency_sol_items))
+            queue.extend(_form_queue_items_for_dependency_other_then_contract(sol_data, contract, dependency_sol_type, dependency_sol_items))
         
         break
 
   return exported_items
 
+def _compute_initial_queue_item(sol_data, export):
+  export_tokens = export.split('.')
 
-def form_queue_items_for_dependent_contract(sol_data, dependent_contracts):
+  if export_tokens[-1] == 'contracts':
+    return [[export_tokens[0]], export_tokens[0], export_tokens[1]]
+
+  return [_find_contract_chain_downwards(export_tokens[0], sol_data), export_tokens[1], export_tokens[2]]
+
+
+def _form_queue_items_for_dependent_contract(sol_data, dependent_contracts):
   queue_items = []
   for dependent_contract in dependent_contracts:
-    for contract_in_chain in find_contract_chain_downwards(dependent_contract, sol_data, False):
+    for contract_in_chain in _find_contract_chain_downwards(dependent_contract, sol_data, False):
       for sol_type, sol_items in sol_data[contract_in_chain].items():
         if sol_type in ["base", "code"]:
           continue
@@ -69,13 +70,13 @@ def form_queue_items_for_dependent_contract(sol_data, dependent_contracts):
   
   return queue_items
 
-
-def form_queue_items_for_dependency_other_then_contract(sol_data, contract, dependency_sol_type, dependency_sol_items):
-  dependency_possible_contracts = find_contract_chain_downwards(contract, sol_data)
+def _form_queue_items_for_dependency_other_then_contract(sol_data, contract, dependency_sol_type, dependency_sol_items):
+  dependency_possible_contracts = _find_contract_chain_downwards(contract, sol_data)
   
   return [[dependency_possible_contracts, dependency_name, dependency_sol_type] for dependency_name in dependency_sol_items]
 
-def find_contract_chain_downwards(contract, sol_data, add_global = True):
+
+def _find_contract_chain_downwards(contract, sol_data, add_global = True):
   contracts = []
   current_contract = contract
   while current_contract is not None:
@@ -87,7 +88,7 @@ def find_contract_chain_downwards(contract, sol_data, add_global = True):
 
   return contracts
 
-def find_contract_chain_upwards(contract, sol_data):
+def _find_contract_chain_upwards(contract, sol_data):
   contracts = []
   current_contract = contract
   while current_contract is not None:
@@ -99,3 +100,4 @@ def find_contract_chain_upwards(contract, sol_data):
     current_contract = new_contract
 
   return contracts
+  
