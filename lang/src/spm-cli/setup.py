774d3e2d865f689e75fbf004cc2429209d31943b
Panic <panic.milos99@gmail.com>
2023-03-22 20:20:52 +0100

    added missing libraries in spm-cli

--- lang/src/spm-cli/setup.py
+++ lang/src/spm-cli/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'Click', 'starkbank-ecdsa'
+        'Click', 'starkbank-ecdsa', 'jinja2', 'requests'
     ],
     entry_points='''
         [console_scripts]
